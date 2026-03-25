bd9f06c63216c7d9c38cddfc47fbdb00c28ada4d
Shlokabhishek <smiletome73280@gmail.com>
2026-03-25 23:53:43 +0530

    chore(api): make http-check synchronous

--- api/http-check.ts
+++ api/http-check.ts
@@ -1,6 +1,6 @@
 import { assertMethod, sendJson, type ApiRequest, type ApiResponse } from './lib/http';
 
-export default async function handler(request: ApiRequest, response: ApiResponse) {
+export default function handler(request: ApiRequest, response: ApiResponse) {
   assertMethod(request.method, ['GET']);
   return sendJson(response, 200, {
     ok: true,
